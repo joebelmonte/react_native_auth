d10c4fdaae337bec40c2ee7c581a60deb7837fe9
joebelmonte <joseph.belmonte@gmail.com>
2017-09-18 22:31:45 -0400

    Added spinner to log in page to indicate async action

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
-import { Button, Card, CardSection, Input } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '', loading: false };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '', loading: true });
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(() => {
+        this.setState({ error: 'Authentication Failed.' });
+      });
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>;
+  }
 
   render() {
     return (
@@ -26,12 +48,25 @@ class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>Log in</Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
